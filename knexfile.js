e38acd06cc77b289a8de53b01e39ffb80ea9812b
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-10-16 13:19:43 -0300

    configurando knex no ambiente e criando a migration com knex

--- knexfile.js
+++ knexfile.js
@@ -5,43 +5,17 @@
  */
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    development: {
+        client: 'pg',
+        connection: {
+            database: 'Knex_test',
+            user: 'postgres',
+            password: '12345678',
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: `${__dirname}/src/database/migrations`
+        }
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
