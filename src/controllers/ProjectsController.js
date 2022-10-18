723a3ab00437c43aec9ad5d1dcd4f88d38fb6a0c
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-10-18 10:21:34 -0300

    paginaçao de projetos

--- src/controllers/ProjectsController.js
+++ src/controllers/ProjectsController.js
@@ -4,18 +4,26 @@ module.exports = {
 
     async index(req, res, next) {
         try {
-            const { user_id } = req.query
+            const { user_id, page = 1 } = req.query
 
             const query = knex('projects')
+                .limit(5)
+                .offset((page - 1) * 5)
 
-
+            const countObj = knex('projects').count()
             if (user_id) {
                 query
                     .where({ user_id })
                     .join('users', 'user_id', '=', 'projects.user_id')
                     .select('projects.*', 'users.username')
 
+                countObj
+                    .where({ user_id })
             }
+            const [count] = await countObj
+
+            res.header('X-total-Count', count["count"])
+
             const results = await query
 
             return res.status(200).json(results)
